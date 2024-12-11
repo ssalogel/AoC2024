a091aad6e88d7fd905b8668e5a2f06d5130ed689
charles <therien.charles+github@gmail.com>
2024-12-11 09:31:22 -0500

    Day 11 refactor

--- day11.py
+++ day11.py
@@ -3,16 +3,7 @@ from math import log10, floor
 from utils import Day
 from collections import Counter
 
-def step(stone: int) -> list[int]:
-    if stone == 0:
-        return [1]
-    stone_size = floor(log10(stone)) + 1
-    if stone_size % 2 == 0:
-        div, mod = divmod(stone, 10 ** (stone_size // 2))
-        return [div, mod]
-    return [stone * 2024]
-
-def step2(stone: int, amount: int) -> dict[int, int]:
+def step(stone: int, amount: int) -> dict[int, int]:
     if stone == 0:
         return {1: amount}
     stone_size = floor(log10(stone)) + 1
@@ -21,28 +12,21 @@ def step2(stone: int, amount: int) -> dict[int, int]:
         return {div: amount, mod: amount} if div != mod else {div: amount*2}
     return {stone * 2024: amount}
 
-
+def iterate(stones: Counter[int], amount: int) -> Counter[int]:
+    for _ in range(amount):
+        next_stones = Counter()
+        for stone, amount in stones.items():
+            next_stones.update(step(stone, amount))
+        stones = next_stones
+    return stones
 
 def part_one(data: list[str]) -> Union[str, int]:
-    stones = [int(x) for x in data[0].split()]
-
-    for _ in range(25):
-        tmp = []
-        for stone in stones:
-            tmp.extend(step(stone))
-        stones = tmp
-
-    return len(stones)
+    stones = Counter(int(x) for x in data[0].split())
+    return iterate(stones, 25).total()
 
 def part_two(data: list[str]) -> Union[str, int]:
-    stones2 = Counter([int(x) for x in data[0].split()])
-
-    for _ in range(75):
-        next_stones = Counter()
-        for stone2, amount in stones2.items():
-            next_stones.update(step2(stone2, amount))
-        stones2 = next_stones
-    return stones2.total()
+    stones = Counter([int(x) for x in data[0].split()])
+    return iterate(stones, 75).total()
 
 
 def main():
