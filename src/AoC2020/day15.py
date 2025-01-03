e05e9d7430d1d7f90fb72859b80e6f73b0cfb5ea
charles <therien.charles+github@gmail.com>
2025-01-03 14:13:41 -0500

    day 15 2020

--- src/AoC2020/day15.py
+++ src/AoC2020/day15.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Union
 from time import perf_counter
 from src.utils import Day
@@ -7,16 +8,21 @@ import logging
 logger = logging.getLogger("AoC")
 
 
-def part_one(data: list[str]) -> Union[str, int]:
-    return data
-
-
-def part_two(data: list[str]) -> Union[str, int]:
-    pass
+def part_one(data: list[str], end: int) -> Union[str, int]:
+    starting_numbers = [int(x) for x in data[0].split(",")]
+    res = [-1] * end  # about twice as fast as using a defaultdict(lambda: -1)
+    for ix, n in enumerate(starting_numbers[:-1]):
+        res[n] = ix
+    n = starting_numbers[-1]
+    for ix in range(len(starting_numbers) - 1, end - 1):
+        t = res[n]
+        res[n] = ix
+        n = ix - t if t != -1 else 0
+    return n
 
 
 def main(test: bool = False):
-    test_case_1 = """"""
+    test_case_1 = """0,3,6"""
 
     day = 15
     if test:
@@ -26,12 +32,12 @@ def main(test: bool = False):
         data = Day.get_data(2020, day).strip().split("\n")
 
     start = perf_counter()
-    logger.info(f"day {day} part 1: {part_one(data)}  in {perf_counter() - start:.4f}s")
+    logger.info(f"day {day} part 1: {part_one(data, 2020)}  in {perf_counter() - start:.4f}s")
     mid = perf_counter()
-    logger.info(f"day {day} part 2: {part_two(data)} in {perf_counter() - mid:.4f}s")
+    logger.info(f"day {day} part 2: {part_one(data, 30000000)} in {perf_counter() - mid:.4f}s")
     logger.info(f"the whole day {day} took {perf_counter() - start:.4f}s")
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.NOTSET, stream=sys.stdout)
-    main(True)
+    main()
