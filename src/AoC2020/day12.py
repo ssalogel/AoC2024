0ebfdec8294f5d78a1f9384a8ddbe42ac1e91fd1
charles <therien.charles+github@gmail.com>
2025-01-03 01:02:01 -0500

    day 12 2020

--- src/AoC2020/day12.py
+++ src/AoC2020/day12.py
@@ -8,15 +8,69 @@ logger = logging.getLogger("AoC")
 
 
 def part_one(data: list[str]) -> Union[str, int]:
-    return data
+    boat = 0
+    direction = 1
+    directions = [1j, 1, -1j, -1]
+    for ix, instr in enumerate(data):
+        if ix == 8:
+            pass
+        op, value = instr[0], int(instr[1:])
+        match op:
+            case "N":
+                boat += value * 1j
+            case "S":
+                boat -= value * 1j
+            case "E":
+                boat += value
+            case "W":
+                boat -= value
+            case "L":
+                direction = (direction - (value//90))%4
+            case "R":
+                direction = (direction + (value//90))%4
+            case "F":
+                boat += value * directions[direction]
+
+    return int(abs(boat.imag) + abs(boat.real))
 
 
 def part_two(data: list[str]) -> Union[str, int]:
-    pass
+    waypoint = 10 + 1j
+    boat = 0
+    direction = 1
+    directions = [1j, 1, -1j, -1]
+    for ix, instr in enumerate(data):
+        if ix == 8:
+            pass
+        op, value = instr[0], int(instr[1:])
+        match op:
+            case "N":
+                waypoint += value * 1j
+            case "S":
+                waypoint -= value * 1j
+            case "E":
+                waypoint += value
+            case "W":
+                waypoint -= value
+            case "L":
+                for _ in range(value//90):
+                    waypoint *= 1j
+            case "R":
+                for _ in range(value//90):
+                    waypoint *= -1j
+            case "F":
+                boat += value * waypoint
+
+    return int(abs(boat.imag) + abs(boat.real))
 
 
 def main(test: bool = False):
-    test_case_1 = """"""
+    test_case_1 = """F10
+N3
+F7
+R90
+F11
+"""
 
     
     day = 12
@@ -34,4 +88,5 @@ def main(test: bool = False):
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.NOTSET, stream=sys.stdout)
-    main(True)
+    main(False)
+
