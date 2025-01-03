6fc9c85e596b91afb870ec562d43bdcd80c35a52
charles <therien.charles+github@gmail.com>
2025-01-03 01:54:24 -0500

    day 13 2020

--- src/AoC2020/day13.py
+++ src/AoC2020/day13.py
@@ -3,20 +3,32 @@ from time import perf_counter
 from src.utils import Day
 import sys
 import logging
+from math import lcm
 
 logger = logging.getLogger("AoC")
 
 
 def part_one(data: list[str]) -> Union[str, int]:
-    return data
+    start = int(data[0])
+    times = [int(x) for x in data[1].split(",") if x != "x"]
+    wait = [x - (start%x) for x in times]
+    best = min(wait)
+    return best * times[wait.index(best)]
 
 
 def part_two(data: list[str]) -> Union[str, int]:
-    pass
+    buses = [(int(x), ix) for ix, x in enumerate(data[1].split(",")) if x != "x"]
+    time = buses[0][0]
+    for ix, (bus, delay) in enumerate(buses[1:]):
+        jmp = lcm(*[t for t, d in buses[:ix+1]])
+        while time % bus != (bus - delay)%bus:
+            time += jmp
+    return time
 
 
 def main(test: bool = False):
-    test_case_1 = """"""
+    test_case_1 = """939
+1789,37,47,1889"""
 
     day = 13
     if test:
@@ -34,4 +46,4 @@ def main(test: bool = False):
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.NOTSET, stream=sys.stdout)
-    main(True)
+    main()
