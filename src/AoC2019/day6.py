5864c719b4ebfeb812b3f5a25ad83a0a3749e8b7
charles <therien.charles+github@gmail.com>
2025-01-08 19:17:00 -0500

    day 6 2019

--- src/AoC2019/day6.py
+++ src/AoC2019/day6.py
@@ -3,20 +3,60 @@ from time import perf_counter
 from src.utils import Day
 import sys
 import logging
+from collections import defaultdict, deque
 
 logger = logging.getLogger("AoC")
 
 
+def calc_num_orbits(orbits, planet: str = "COM", v: int = 0) -> int:
+    if len(orbits[planet]) == 0:
+        return v
+    return sum(calc_num_orbits(orbits, x, v + 1) for x in orbits[planet]) + v
+
+
 def part_one(data: list[str]) -> Union[str, int]:
-    return data
+    orbits = defaultdict(list)
+    for line in data:
+        a, b = line.split(")", 2)
+        orbits[a].append(b)
+
+    return calc_num_orbits(orbits)
 
 
 def part_two(data: list[str]) -> Union[str, int]:
-    pass
+    transfers = defaultdict(list)
+    for line in data:
+        a, b = line.split(")", 2)
+        transfers[a].append(b)
+        transfers[b].append(a)
+
+    visited = set()
+    to_visit = deque()
+    to_visit.append((-1, "YOU"))
+    while True:
+        cost, pos = to_visit.popleft()
+        visited.add(pos)
+        for npos in transfers[pos]:
+            if npos == "SAN":
+                return cost
+            if npos not in visited:
+                to_visit.append((cost + 1, npos))
 
 
 def main(test: bool = False):
-    test_case_1 = """"""
+    test_case_1 = """COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN"""
 
     day = 6
     if test:
@@ -36,4 +76,4 @@ def main(test: bool = False):
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.NOTSET, stream=sys.stdout)
-    main(True)
+    main()
